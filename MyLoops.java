4c44b194f1d87cb03ff01a5d83a4142b25b9f5d1
Noubiyoyo <geoffroyolougo@gmail.com>
2022-06-11 21:52:12 +0100

    Boucles

---
+++ MyLoops.java
@@ -0,0 +1,22 @@
+public class MyLoops {
+    
+    public static void main(String[] args) {
+        int i=0;
+      /*  for(int i=0 ; i<5;i++)
+        {
+            System.out.println("Je m'amuse tellement !");
+        }
+        */
+        while(i<5)
+        {
+            if(i==3)
+            {
+                System.out.println("Je m'amuse tellement !");
+            }
+             i++;
+
+        }
+
+        
+    }
+}
